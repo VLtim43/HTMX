dcff0760531a61687cc3f29648fdddefb32aa7ce
VLtim43 <fernandoreal.valentim@gmail.com>
2023-12-27 15:31:26 -0300

    It works

--- src/index.tsx
+++ src/index.tsx
@@ -10,10 +10,15 @@ const app = new Elysia()
   .get("/", ({ html }) =>
     html(
       <Layout>
-        <p>im a children</p>
+        <div>
+          <button hx-post="/clicked" hx-swap="outerHTML">
+            Click me
+          </button>
+        </div>
       </Layout>
     )
   )
+  .post("/clicked", () => <div>im from the server!</div>)
   .listen(3000);
 
 export function Layout(props: Html.PropsWithChildren<{ title?: string }>) {
@@ -27,6 +32,7 @@ export function Layout(props: Html.PropsWithChildren<{ title?: string }>) {
             name="viewport"
             content="width=device-width, initial-scale=1.0"
           />
+          <script src="https://unpkg.com/htmx.org@1.9.10"></script>
           <title>{props.title || "Hello World!"}</title>
         </head>
         <body>{props.children}</body>
